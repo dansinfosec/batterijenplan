e3a1a60271b238dbfa18e0784bd67c98451fb1c7
Daniel Schuiten <ecomdaniel266@gmail.com>
2026-07-04 12:26:19 +0200

    Add Google Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
 import PostDetail from "./pages/PostDetail.jsx";
 import Calculator from "./pages/Calculator.jsx";
 import Privacy from "./pages/Privacy.jsx";
+import { initAnalytics, trackPageView } from "./analytics.js";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    initAnalytics();
+  }, []);
+
+  useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+      <AnalyticsTracker />
+
       <Header />
+
       <main style={{ flex: 1 }}>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -18,6 +38,7 @@ export default function App() {
           <Route path="/privacy" element={<Privacy />} />
         </Routes>
       </main>
+
       <Footer />
     </div>
   );
